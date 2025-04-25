bc06af1a928d8e78f56c58ffd544e32aa783d42e
MarvinC <157068468+MarvinC21@users.noreply.github.com>
2025-04-25 02:30:17 -0500

    added pins to map when event is clicked
    still need to figure out zooming in on pin (mainly QOL)

--- MacEvents/EventCoordinatePins.swift
+++ MacEvents/EventCoordinatePins.swift
@@ -10,22 +10,52 @@ import SwiftUI
 import MapKit
 
 struct EventCoordinatePins: View {
+    @State var selectedEvent: Event? = nil
     @State var events: [Event] = []
-   
+    @State var eventCoord: CLLocationCoordinate2D?
+    
+    private var region: MKCoordinateRegion =
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 44.938056, longitude:-93.168494),
+            span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        )
+    
     var body: some View {
-        Map(initialPosition: .region(region))
-        
-        VStack {
-            ForEach(events) { event in
-                Text(event.title)
+        ScrollView {
+            ForEach($events) { event in
+                Button(action: {
+                    selectedEvent = event.wrappedValue
+                    print("Current selected event: \(selectedEvent!.title)")
+                    if event.wrappedValue.coord != nil {
+                        eventCoord = CLLocationCoordinate2D(latitude: event.wrappedValue.coord![0], longitude: event.wrappedValue.coord![1])
+                        print("Current event coord: \(selectedEvent!.coord)")
+                    } else {
+                        eventCoord = nil
+                    }
+                }) {
+                    Text(event.wrappedValue.title)
+                }
             }
         }
+        
+        Map(initialPosition: .region(region),
+            bounds: MapCameraBounds(centerCoordinateBounds: region,
+                                    minimumDistance: 450,
+                                    maximumDistance: 450)) {
+            if eventCoord != nil && selectedEvent != nil {
+                Marker(coordinate: eventCoord!) {
+                    Text(selectedEvent!.location)
+                }
+            }
+        }
+        
+        
         
         .task {
             do {
                 let url = URL(string: "http://127.0.0.1:5000/events")!
                 let (data, _) = try await URLSession.shared.data(from: url)
-                    
+                
                 let eventData = try JSONDecoder().decode([Event].self, from: data)
                 events = eventData
                 
@@ -35,13 +65,6 @@ struct EventCoordinatePins: View {
             }
         }
     }
-    private var region: MKCoordinateRegion{
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 44.938056, longitude:-93.168494),
-            span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
-        )
-    }
-    
 }
 
 #Preview {
