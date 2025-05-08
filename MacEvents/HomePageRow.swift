7624171592438cb58dab35a2604b7e5a05742df8
MarvinC <157068468+MarvinC21@users.noreply.github.com>
2025-05-08 10:49:59 -0500

    fixed syntax error

--- MacEvents/HomePageRow.swift
+++ MacEvents/HomePageRow.swift
@@ -34,11 +34,9 @@ struct HomePageRow: View {
                 }
                        
             }
-                    
-        }
         .frame(height: 600)
+        }
     }
-}
 
 
 #Preview {
